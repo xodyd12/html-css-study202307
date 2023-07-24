62dc5059b5cf3c7a803ae6ff84356b3c4115bb08
Hantaeyong <hhhhgong3@naver.com>
2023-07-24 09:40:06 +0900

    css/responsive

---
+++ CSS-template/js/gnb.event.js
@@ -0,0 +1,13 @@
+const menuBtn = document.querySelector('header .menu-open');
+const closeBtn = document.querySelector('.gnb .close');
+
+const gnb = document.querySelector('.gnb');
+
+//클릭 이벤트 생성
+menuBtn.addEventListener('click', () => {
+    gnb.classList.add('open');
+});
+
+closeBtn.addEventListener('click', () => {
+    gnb.classList.remove('open');
+});
